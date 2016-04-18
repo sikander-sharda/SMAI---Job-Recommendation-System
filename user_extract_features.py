d0df76ab19dae682c0e45e8c728e12fd283e5ee2
nmcsk <naman.singhal02@gmail.com>
2016-04-19 02:15:27 +0530

    Features for Clustering

--- user_extract_features.py
+++ user_extract_features.py
@@ -1,11 +1,15 @@
 f = open('users.csv','r')
 f.readline()
-out = open('users_without_last.csv','w')
+out = open('user_rel_features.csv','w')
 for line in f:
     line_details = line.split()
     line_details[len(line_details)-1] = line_details[len(line_details)-1].split('\n')[0]
     for i in range(11):
-        out.write(line_details[i] + ',,,,')
+        if i!= 1:
+            if i!=10:
+                out.write(line_details[i] + ',')
+            else:
+                out.write(line_details[i])
     out.write('\n')
 f.close()
 out.close()
