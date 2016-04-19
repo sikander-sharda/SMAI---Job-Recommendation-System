9c6cfbe4993bbd2884d0e5afe5bc4d78bbc3d150
nmcsk <naman.singhal02@gmail.com>
2016-04-19 22:07:01 +0530

    10:00

--- preprocess.py
+++ preprocess.py
@@ -5,12 +5,12 @@ career_level = [1,2,3,4,5,6]
 
 for user in inp:
     user_details = user.split('\n')[0].split(',')
-    if user_details[1] == 0:
+    if user_details[1] == "0":
         for i in career_level:
-            out.write(user_details[0] + ',' + i + ',')
+            out.write(user_details[0] + ',' + str(i) + ',')
             for j in range(2,len(user_details)-1):
                 out.write(user_details[j] + ',')
-            out.write(user_detils[len(user_details)-1])
+            out.write(user_details[len(user_details)-1])
             out.write('\n')
     else:
         out.write(user)
